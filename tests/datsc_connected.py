b00d7f4fef38aa2379a69757bc95a8e036e4adfa
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-04-19 14:31:53 -0500

    works? :

--- tests/datsc_connected.py
+++ tests/datsc_connected.py
@@ -58,14 +58,19 @@ from datetime import datetime
 sys.path.append("..")
 from Resource_Share.resource_manager import ResourceManager
 
+def slow_write(serial_port:serial.Serial, str:bytes):
+    for c in str:
+        serial_port.write(c)
+        time.sleep(0.1)
+
 
 def advertising_test(serial_port: serial.Serial):
     print("Advertising test")
     start = datetime.now()
 
     while True:
-        text = str(serial_port.readline())
-
+        text = serial_port.readline().decode('utf-8')
+    
         if "Advertising started" in text:
             return True
         elif (datetime.now() - start).total_seconds() > 10:
@@ -80,12 +85,14 @@ def test_secure_connection(serial_port: serial.Serial):
 
     while True:
         text = str(serial_port.readline())
-        print(text)
+        # print(text)
         # wait until you see the term passkey, so we can enter the pin
         if "enter passkey" in text:
             time.sleep(1)
-            serial_port.write("pin 1 1234\r\n".encode("utf-8"))
+            serial_port.write("pin 1 1234\n".encode("utf-8"))
             break
+        elif 'Connection encrypted' in text:
+            return True
 
         if (datetime.now() - start).total_seconds() > 10:
             print("TIMEOUT!!")
@@ -93,14 +100,17 @@ def test_secure_connection(serial_port: serial.Serial):
 
     start = datetime.now()
     while True:
-        text = str(serial_port.readline())
+        text = serial_port.readline().decode('utf-8')
+
         # wait for pairing process to go through and see if it passed or failed
         if "Pairing failed" in text:
             print("Pairing failed")
             return False
-        elif "Pairing completed successfully" in text:
+        elif "Pairing completed successfully" in text or 'Connection encrypted' in text:
             print("Pairing success")
             return True
+        elif 'encryption failed':
+            return False
 
         if (datetime.now() - start).total_seconds() > 10:
             print("TIMEOUT!!")
@@ -108,45 +118,69 @@ def test_secure_connection(serial_port: serial.Serial):
 
 
 def write_char_test(serial_port: serial.Serial):
+    
     print("WRITE CHARACTERISTIC TEST")
-    serial_port.write("btn 2 l\r\n".encode("utf-8"))
+    serial_port.flush()
+    serial_port.write("btn 2 l\n".encode("utf-8"))
     start = datetime.now()
 
     while True:
-        text = str(serial_port.readline())
-        if "No action assigned" in text:
-            return False
-        elif "hello" in text:
-            return True
-        elif (datetime.now() - start).total_seconds() > 10:
-            print("TIMEOUT!!")
-            return False
+        while serial_port.in_waiting:
+            print('x')
+            text = serial_port.readline().decode('utf-8')
+            if "No action assigned" in text:
+                return False
+            elif "hello" in text:
+                return True
+            elif (datetime.now() - start).total_seconds() > 10:
+                print("TIMEOUT!!")
+                return False
+
+        time.sleep(0.5)    
+        serial_port.write("btn 2 l\n".encode("utf-8"))
+        
+    
+
+
 
 
 def write_secure_test(serial_port: serial.Serial):
     print("WRITE SECURE TEST")
+    time.sleep(6)
+    serial_port.flush()
     serial_port.write("btn 2 m\r\n".encode("utf-8"))
+    
+    # serial_port.write("btn 2 m\n".encode("utf-8"))
+    # slow_write(serial_port, "btn 2 m\n".encode("utf-8"))
     start = datetime.now()
 
     while True:
-        text = str(serial_port.readline())
-        if "No action assigned" in text:
-            print(text)
-            return False
-        elif "hello" in text:
-            return True
-        elif (datetime.now() - start).total_seconds() > 10:
-            print("TIMEOUT!!")
-            return False
+        while serial_port.in_waiting:
+            text = serial_port.readline().decode('utf-8')
+            if "No action assigned" in text:
+                return False
+            elif "hello" in text or 'Secure data received!' in text:
+                return True
+            elif (datetime.now() - start).total_seconds() > 10:
+                print("TIMEOUT!!")
+                return False
+        time.sleep(0.5)
+        serial_port.write("btn 2 m\r\n".encode("utf-8"))
+        # slow_write(serial_port, "btn 2 m\r\n".encode("utf-8"))
+
+
 
 
 def phy_switch_test(serial_port: serial.Serial):
     print("PHY CHANGE TEST")
-    serial_port.write("btn 2 s\r\n".encode("utf-8"))
+    serial_port.flush()
+    serial_port.write("btn 2 s\n".encode("utf-8"))
     start = datetime.now()
 
     while True:
-        text = str(serial_port.readline())
+        text = serial_port.readline().decode('utf-8')
+
+        print(text)
         if "No action assigned" in text:
             return False
         elif "2 MBit TX and RX PHY Requested" in text:
@@ -155,10 +189,16 @@ def phy_switch_test(serial_port: serial.Serial):
             print("TIMEOUT!!")
             return False
 
+        time.sleep(0.5)
+        serial_port.write("btn 2 s\n".encode("utf-8"))
+
+        
+
 
 def speed_test(serial_port: serial.Serial):
     print("SPEED TEST")
-    serial_port.write("btn 2 x\r\n".encode("utf-8"))
+    serial_port.flush()
+    serial_port.write("btn 2 x\n".encode("utf-8"))
     start = datetime.now()
 
     try:
@@ -167,10 +207,13 @@ def speed_test(serial_port: serial.Serial):
         return False
 
     time.sleep(2)
-    serial_port.write("btn 2 m\r\n".encode("utf-8"))
+    serial_port.write("btn 2 m\n".encode("utf-8"))
 
     while True:
-        text = str(serial_port.readline())
+        
+        text = serial_port.read(serial_port.in_waiting).decode('utf-8')
+
+        print(text)
         if "bps" in text:
             print(text)
             return True
@@ -185,10 +228,15 @@ test_results_client = {}
 def client_thread(portname: str):
     client_console = serial.Serial(portname, baudrate=115200, timeout=2)
     test_results_client["pairing"] = test_secure_connection(client_console)
+    if not test_results_client['pairing']:
+        return
     test_results_client["write characteristic"] = write_char_test(client_console)
     test_results_client["write secure"] = write_secure_test(client_console)
-    test_results_client['phy switch'] = phy_switch_test(client_console)
+
+    test_results_client["phy switch"] = phy_switch_test(client_console)
     test_results_client["speed"] = speed_test(client_console)
+
+    client_console.flush()
 
     return test_results_client
 
@@ -210,14 +258,13 @@ def print_results(name, report):
     print(f"{name} RESULTS")
     print(f"{'TEST':<25} Result")
     for key, value in report.items():
-        print('-'*30)
+        print("-" * 30)
         print(f"{key:<25}{'Fail' if not value else 'Pass'}")
 
         if not value:
             overall = False
 
-    print('-'*30, '\n')
-    
+    print("-" * 30, "\n")
 
     return overall
 
@@ -226,7 +273,6 @@ if __name__ == "__main__":
     if len(sys.argv) < 5:
         print("Not enough arguments!")
         sys.exit(-1)
-
 
     rm = ResourceManager()
 
@@ -237,32 +283,35 @@ if __name__ == "__main__":
     datc_file = sys.argv[4]
 
     # Make sure all bonding information is wiped
-    rm.resource_erase(server_board)
-    rm.resource_erase(client_board)
+    # rm.resource_erase(server_board)
+    # rm.resource_erase(client_board)
 
-    
-    rm.resource_flash(server_board, dats_file)
-    rm.resource_flash(client_board, datc_file)
+    # rm.resource_flash(server_board, dats_file)
+    # rm.resource_flash(client_board, datc_file)
 
     # Get console ports associated with the boards
     server_port = rm.get_item_value(f"{server_board}.console_port")
     client_port = rm.get_item_value(f"{client_board}.console_port")
 
+    assert client_port != server_port
+
     # Configure and run tests
     client_t = threading.Thread(target=client_thread, args=(client_port,))
     server_t = threading.Thread(target=server_thread, args=(server_port,))
-    client_t.start()
-    server_t.start()
-
+    
     # Reset to start from scratch
     rm.resource_reset(server_board)
     rm.resource_reset(client_board)
+    
+    client_t.start()
+    # server_t.start()
+
 
     client_t.join()
-    server_t.join()
+    # server_t.join()
 
     # Print Results
-    print('\n\n')
+    print("\n\n")
     overall_client = print_results("DATC", test_results_client)
     overall_server = print_results("DATS", test_results_server)
 
