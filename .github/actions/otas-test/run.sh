ad35ad1a48d89bd0dabe62fc6b2af4407f04f6d3
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-04-22 16:49:34 -0500

    debug

--- .github/actions/otas-test/run.sh
+++ .github/actions/otas-test/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BOARD_SERVER="${INPUT_BOARD_SERVER}"
-BOARD_CLIENT="${INPUT_BOARD_CLIENT}"
+BOARD_SERVER=${INPUT_BOARD_SERVER}
+BOARD_CLIENT=${INPUT_BOARD_CLIENT}
 
-target_server=$(resource_manager.py -g ${BOARD_SERVER}.target)
-target_client=$(resource_manager.py -g ${BOARD_CLIENT}.target)
+target_server=$(resource_manager.py -g $BOARD_SERVER.target)
+target_client=$(resource_manager.py -g $BOARD_CLIENT.target)
 target_server_lower=$(lower ${target_server})
 target_client_lower=$(lower ${target_client})
 
@@ -12,4 +12,5 @@ msdk_path=${INPUT_MSDK_PATH}
 elf_server=${msdk_path}/Examples/${target_server}/Bluetooth/BLE_otas/build/${target_server_lower}.elf
 elf_client=${msdk_path}/Examples/${target_client}/Bluetooth/BLE_otas/build/${target_client_lower}.elf
 
-../../../tests/otas_connected.py $target_server $target_client $elf_server $elf_client
+
+cd ../../../tests && python3 otas_connected.py $target_server $target_client $elf_server $elf_client
