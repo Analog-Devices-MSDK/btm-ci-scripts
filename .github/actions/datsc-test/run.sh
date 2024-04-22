141bf7dd9126893d45708a682975bc5b79b03381
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-04-22 15:55:49 -0500

    updated dat test action

---
+++ .github/actions/datsc-test/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BOARD_SERVER="${{ inputs.board_server }}"
+BOARD_CLIENT="${{ inputs.board_client }}"
+
+target_server=$(resource_manager.py -g ${BOARD_SERVER}.target)
+target_client=$(resource_manager.py -g ${BOARD_CLIENT}.target)
+target_server_lower=$(lower ${target_server})
+target_client_lower=$(lower ${target_client})
+
+msdk_path="${{ inputs.msdk_path }}"
+elf_server=${msdk_path}/Examples/${target_server}/Bluetooth/BLE_otas/build/${target_server_lower}.elf
+elf_client=${msdk_path}/Examples/${target_client}/Bluetooth/BLE_otas/build/${target_client_lower}.elf
+
+
+../../../tests/datsc_connected.py $target_server $target_client $elf_server $elf_client
