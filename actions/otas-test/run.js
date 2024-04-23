90f59ce5b484ea3a1e60b5a3fd0f740e193045b3
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-04-23 16:58:10 -0500

    test env

--- actions/otas-test/run.js
+++ actions/otas-test/run.js
@@ -1,119 +1,125 @@
-const fs = require('fs')
-const execSync = require('child_process').execSync;
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { exit } = require('process');
-const shell = require('python-shell')
+
+console.log(process.env.CI_BOARD_CONFIG)
+console.log(process.env.OPENOCD_PATH)
+console.log(process.env.RESOURCE_LOCK_DIR)
+
+
+// const fs = require('fs')
+// const execSync = require('child_process').execSync;
+// const core = require('@actions/core');
+// const github = require('@actions/github');
+// const { exit } = require('process');
+// const shell = require('python-shell')
 
 
 
 
-const BOARD_SERVER = core.getInput('board_server');
-const BOARD_CLIENT = core.getInput('board_client');
-const MSDK_PATH = core.getInput('msdk_path');
+// const BOARD_SERVER = core.getInput('board_server');
+// const BOARD_CLIENT = core.getInput('board_client');
+// const MSDK_PATH = core.getInput('msdk_path');
 
-async function resource_manager_get(option){
+// async function resource_manager_get(option){
     
-    let options = {
-        mode: 'text',
-        pythonPath: 'path/to/python',
-        pythonOptions: ['-u'], // get print results in real-time
-        scriptPath: '../../Resource_Share',
-        args: ['-g', option]
-      };
+//     let options = {
+//         mode: 'text',
+//         pythonPath: 'path/to/python',
+//         pythonOptions: ['-u'], // get print results in real-time
+//         scriptPath: '../../Resource_Share',
+//         args: ['-g', option]
+//       };
 
-      const { success, err = '', results } = await new Promise(
-        (resolve, reject) =>
-        {
-            PythonShell.run('hello.py', options,
-                function (err, results)
-                {
-                    if (err)
-                    {
-                        reject({ success: false, err });
-                    }
+//       const { success, err = '', results } = await new Promise(
+//         (resolve, reject) =>
+//         {
+//             PythonShell.run('hello.py', options,
+//                 function (err, results)
+//                 {
+//                     if (err)
+//                     {
+//                         reject({ success: false, err });
+//                     }
 
-                    console.log('PythonShell results: %j', results);
+//                     console.log('PythonShell results: %j', results);
 
-                    resolve({ success: true, results });
-                }
-            );
-        }
-    );
+//                     resolve({ success: true, results });
+//                 }
+//             );
+//         }
+//     );
       
-    return success, err, results;
+//     return success, err, results;
 
-}
-
-
-console.log(BOARD_SERVER);
-console.log(BOARD_CLIENT);
-console.log(MSDK_PATH);
+// }
 
 
-console.log(process.env);
+// console.log(BOARD_SERVER);
+// console.log(BOARD_CLIENT);
+// console.log(MSDK_PATH);
 
 
-
-let command = 'resource_manager.py -g ${BOARD_CLIENT}.target';
-console.log(command)
-
-success, err, target_client = await resource_manager_get("${BOARD_CLIENT}.target");
+// console.log(process.env);
 
 
 
+// let command = 'resource_manager.py -g ${BOARD_CLIENT}.target';
+// console.log(command)
 
-// let target_client = execSync(command).stdout;
-console.log(target_client);
-
-
-let target_server = execSync("resource_manager.py -g ${BOARD_SERVER}.target");
-
-let target_client_obj_lower = target_client_obj.toLowerCase();
-let target_server_obj_lower = target_server_obj.toLowerCase();
+// success, err, target_client = await resource_manager_get("${BOARD_CLIENT}.target");
 
 
 
 
-console.log(target_client_obj);
-console.log(target_server_obj);
+// // let target_client = execSync(command).stdout;
+// console.log(target_client);
 
 
-const OTAS_PATH = "${MSDK_PATH}/Examples/${target_server}/Bluetooth/BLE_otas"
-const OTAC_PATH = "${MSDK_PATH}/Examples/${target_client}/Bluetooth/BLE_otac"
+// let target_server = execSync("resource_manager.py -g ${BOARD_SERVER}.target");
 
-execSync("make -j -C ${OTAS_PATH}");
-execSync("make -j -C ${OTAC_PATH}");
-
-
-const elf_server = "${OTAS_PATH}/build/${target_lower}.elf"
-const elf_client = "${OTAC_PATH}/build/${target_lower}.elf"
-
-
-if(!fs.existsSync(elf_server))
-{
-    console.log("${elf_server} DNE!")
-    exit(-1);
-}
-else if(!fs.existsSync(elf_client))
-{
-    console.log("${elf_client} DNE!")
-    exit(-1);
-}
-
-command = "cd ../../../tests && python3 otas_connected.py ${target_server} ${target_client} ${elf_server} ${elf_client}"
-
-let ret = execSync(command);
-
-if(ret )
+// let target_client_obj_lower = target_client_obj.toLowerCase();
+// let target_server_obj_lower = target_server_obj.toLowerCase();
 
 
 
-exec(command,
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    });
+
+// console.log(target_client_obj);
+// console.log(target_server_obj);
+
+
+// const OTAS_PATH = "${MSDK_PATH}/Examples/${target_server}/Bluetooth/BLE_otas"
+// const OTAC_PATH = "${MSDK_PATH}/Examples/${target_client}/Bluetooth/BLE_otac"
+
+// execSync("make -j -C ${OTAS_PATH}");
+// execSync("make -j -C ${OTAC_PATH}");
+
+
+// const elf_server = "${OTAS_PATH}/build/${target_lower}.elf"
+// const elf_client = "${OTAC_PATH}/build/${target_lower}.elf"
+
+
+// if(!fs.existsSync(elf_server))
+// {
+//     console.log("${elf_server} DNE!")
+//     exit(-1);
+// }
+// else if(!fs.existsSync(elf_client))
+// {
+//     console.log("${elf_client} DNE!")
+//     exit(-1);
+// }
+
+// command = "cd ../../../tests && python3 otas_connected.py ${target_server} ${target_client} ${elf_server} ${elf_client}"
+
+// let ret = execSync(command);
+
+// if(ret )
+
+
+
+// exec(command,
+//     function (error, stdout, stderr) {
+//         console.log('stdout: ' + stdout);
+//         console.log('stderr: ' + stderr);
+//         if (error !== null) {
+//             console.log('exec error: ' + error);
+//         }
+//     });
