fbe5b2a8aec03a0e9b3d488431f92839536db36e
abby wolf <Abigail.Wolf@analog.com>
2024-04-25 10:52:35 -0500

    testing

--- actions/ocderase/run.js
+++ actions/ocderase/run.js
@@ -84,9 +84,13 @@ const eraseAborted = function (val) {
 
 const main = async function () {
     console.log('starting+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    let owner = await getBoardOwner(BOARD_ID);
-    console.log('owner received');
-    console.log(owner);
+    try {
+        let owner = await getBoardOwner(BOARD_ID);
+        console.log('owner received');
+        console.log(owner);
+    } catch (error) {
+        console.error(error);
+    }
     if (owner === OWNER_REF) {
         let target = await getBoardData(BOARD_ID, 'target');
         let dapSN = await getBoardData(BOARD_ID, 'dap_sn');
