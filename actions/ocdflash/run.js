8855208f67ef6b3f45b9c8942f3ec69a654af030
abby wolf <Abigail.Wolf@analog.com>
2024-04-25 16:43:24 -0500

    add make-project action

--- actions/ocdflash/run.js
+++ actions/ocdflash/run.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { spawn } = require('child_process');
 const { env } = require('node:process');
 const { getBoardData, getBoardOwner, procSuccess, procFail } = require('../common');
-// const { makeProject } = require('../make-project/');
+const { makeProject } = require('../make-project/');
 
 const BOARD_ID = Core.getInput('board');
 const PROJECT_DIR = Core.getInput('project');
@@ -13,41 +13,41 @@ const BUILD_FLAG = Core.getBooleanInput('build', { required: false });
 const DISTCLEAN_FLAG = Core.getBooleanInput('distclean', { required : false });
 const OWNER_REF = Github.context.ref;
 
-const makeClean = function (projectPath) {
-    return new Promise((resolve, reject) => {
-        const cleanCmd = spawn('make', ['-C', projectPath, 'clean']);
-        cleanCmd.stdout.on('data', data => { console.log(data.toString().trim()) });
-        cleanCmd.stderr.on('data', data => { console.log(data.toString().trim()) });
-        cleanCmd.on('error', error => {
-            console.error(`ERROR: ${error.message}`);
-        });
-        cleanCmd.on('close', code => {
-            console.log(`Process exited with code ${code}`);
-            if (code != 0) reject(code);
-            else {
-                resolve(code);
-            }
-        });
-    });
-}
+// const makeClean = function (projectPath) {
+//     return new Promise((resolve, reject) => {
+//         const cleanCmd = spawn('make', ['-C', projectPath, 'clean']);
+//         cleanCmd.stdout.on('data', data => { console.log(data.toString().trim()) });
+//         cleanCmd.stderr.on('data', data => { console.log(data.toString().trim()) });
+//         cleanCmd.on('error', error => {
+//             console.error(`ERROR: ${error.message}`);
+//         });
+//         cleanCmd.on('close', code => {
+//             console.log(`Process exited with code ${code}`);
+//             if (code != 0) reject(code);
+//             else {
+//                 resolve(code);
+//             }
+//         });
+//     });
+// }
 
-const makeProject = function (projectPath) {
-    return new Promise((resolve, reject) => {
-        const makeCmd = spawn('make', ['-j', '-C', projectPath]);
-        makeCmd.stdout.on('data', data => { console.log(data.toString().trim()) });
-        makeCmd.stderr.on('data', data => { console.log(data.toString().trim()) });
-        makeCmd.on('error', error => {
-            console.error(`ERROR: ${error.message}`);
-        });
-        makeCmd.on('close', code => {
-            console.log(`Process exited with code ${code}`);
-            if (code != 0) reject(code);
-            else {
-                resolve(code);
-            }
-        });
-    });
-}
+// const makeProject = function (projectPath) {
+//     return new Promise((resolve, reject) => {
+//         const makeCmd = spawn('make', ['-j', '-C', projectPath]);
+//         makeCmd.stdout.on('data', data => { console.log(data.toString().trim()) });
+//         makeCmd.stderr.on('data', data => { console.log(data.toString().trim()) });
+//         makeCmd.on('error', error => {
+//             console.error(`ERROR: ${error.message}`);
+//         });
+//         makeCmd.on('close', code => {
+//             console.log(`Process exited with code ${code}`);
+//             if (code != 0) reject(code);
+//             else {
+//                 resolve(code);
+//             }
+//         });
+//     });
+// }
 
 const flashBoard = function (target, elf, dap, gdb, tcl, telnet) {
     const args = [
@@ -84,11 +84,11 @@ const main = async function () {
             getBoardData(BOARD_ID, 'ocdports.tcl'),
             getBoardData(BOARD_ID, 'ocdports.telnet'),
         ]);
-        let projectPath = path.join(MSDK_PATH, 'Examples', target, 'Bluetooth', PROJECT_DIR);
+        const _projectPath = path.join(MSDK_PATH, 'Examples', target, 'Bluetooth', PROJECT_DIR);
         if (BUILD_FLAG) {
-            // await makeProject(projectPath, DISTCLEAN_FLAG);
-            await makeClean(projectPath);
-            await makeProject(projectPath);
+            await makeProject(_projectPath, DISTCLEAN_FLAG);
+            // await makeClean(projectPath);
+            // await makeProject(_projectPath);
         }
         let elfPath = path.join(projectPath, 'build', `${target.toLowerCase()}.elf`);
         retCode = await flashBoard(target, elfPath, dapSN, gdbPort, tclPort, telnetPort).then(
