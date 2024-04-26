d3053239e5cb5c7d83d8c5b0c228adbaafacbe76
abby wolf <Abigail.Wolf@analog.com>
2024-04-26 11:51:34 -0500

    update flash action

--- actions/ocdflash/run.js
+++ actions/ocdflash/run.js
@@ -13,42 +13,6 @@ const BUILD_FLAG = Core.getBooleanInput('build', { required: false });
 const DISTCLEAN_FLAG = Core.getBooleanInput('distclean', { required : false });
 const OWNER_REF = Github.context.ref;
 
-// const makeClean = function (projectPath) {
-//     return new Promise((resolve, reject) => {
-//         const cleanCmd = spawn('make', ['-C', projectPath, 'clean']);
-//         cleanCmd.stdout.on('data', data => { console.log(data.toString().trim()) });
-//         cleanCmd.stderr.on('data', data => { console.log(data.toString().trim()) });
-//         cleanCmd.on('error', error => {
-//             console.error(`ERROR: ${error.message}`);
-//         });
-//         cleanCmd.on('close', code => {
-//             console.log(`Process exited with code ${code}`);
-//             if (code != 0) reject(code);
-//             else {
-//                 resolve(code);
-//             }
-//         });
-//     });
-// }
-
-// const makeProject = function (projectPath) {
-//     return new Promise((resolve, reject) => {
-//         const makeCmd = spawn('make', ['-j', '-C', projectPath]);
-//         makeCmd.stdout.on('data', data => { console.log(data.toString().trim()) });
-//         makeCmd.stderr.on('data', data => { console.log(data.toString().trim()) });
-//         makeCmd.on('error', error => {
-//             console.error(`ERROR: ${error.message}`);
-//         });
-//         makeCmd.on('close', code => {
-//             console.log(`Process exited with code ${code}`);
-//             if (code != 0) reject(code);
-//             else {
-//                 resolve(code);
-//             }
-//         });
-//     });
-// }
-
 const flashBoard = function (target, elf, dap, gdb, tcl, telnet) {
     const args = [
         '-s', `${env.OPENOCD_PATH}`, '-f', 'interface/cmsis-dap.cfg',
@@ -56,9 +20,10 @@ const flashBoard = function (target, elf, dap, gdb, tcl, telnet) {
         '-c', `gdb_port ${gdb}`, '-c', `telnet_port ${telnet}`, '-c', `tcl_port ${tcl}`,
         '-c', `program ${elf} verify; reset; exit`
     ];
+    let logOut = '';
     return new Promise((resolve, reject) => {
         const flashCmd = spawn('openocd', args);
-        flashCmd.stdout.on('data', data => { console.log(data.toString().trim()) });
+        flashCmd.stdout.on('data', data => { logOut = `${logOut}${data.toString()}` });
         flashCmd.stderr.on('data', data => { console.log(data.toString().trim()) });
         flashCmd.on('error', error => {
             console.error(`ERROR: ${error.message}`);
@@ -75,8 +40,6 @@ const flashBoard = function (target, elf, dap, gdb, tcl, telnet) {
 
 const main = async function () {
     let owner = await getBoardOwner(BOARD_ID);
-    var projectPath;
-    console.log("CODE START");
     if (owner === OWNER_REF) {
         let [target, dapSN, gdbPort, tclPort, telnetPort] = await Promise.all([
             getBoardData(BOARD_ID, 'target'),
@@ -85,17 +48,8 @@ const main = async function () {
             getBoardData(BOARD_ID, 'ocdports.tcl'),
             getBoardData(BOARD_ID, 'ocdports.telnet'),
         ]);
-        // let target = await getBoardData(BOARD_ID, 'target');
-        // let dapSN = await getBoardData(BOARD_ID, 'dap_sn');
-        // let gdbPort = await getBoardData(BOARD_ID, 'ocdports.gdb');
-        // let tclPort = await getBoardData(BOARD_ID, 'ocdports.tcl');
-        // let telnetPort = await getBoardData(BOARD_ID, 'ocdports.telnet');
-        projectPath = path.join(MSDK_PATH, 'Examples', target, 'Bluetooth', PROJECT_DIR);
-        console.log(target);
-        console.log(dapSN);
+        let projectPath = path.join(MSDK_PATH, 'Examples', target, 'Bluetooth', PROJECT_DIR);
         if (BUILD_FLAG) {
-            console.log("PROJECT PATH: %s", projectPath);
-            // await cleanProject(projectPath, DISTCLEAN_FLAG);
             await makeProject(projectPath, DISTCLEAN_FLAG);
         }
         let elfPath = path.join(projectPath, 'build', `${target.toLowerCase()}.elf`);
@@ -113,42 +67,5 @@ const main = async function () {
         console.log("!! ERROR: Improper permissions. Board could not be flashed. !!");
     }
 }
-// const main = async function () {
-//     var projectPath;
-//     let owner = await getBoardOwner(BOARD_ID);
-//     if (owner === OWNER_REF) {
-//         let [target, dapSN, gdbPort, tclPort, telnetPort] = Promise.all([
-//             getBoardData(BOARD_ID, 'target'),
-//             getBoardData(BOARD_ID, 'dap_sn'),
-//             getBoardData(BOARD_ID, 'ocdports.gdb'),
-//             getBoardData(BOARD_ID, 'ocdports.tcl'),
-//             getBoardData(BOARD_ID, 'ocdports.telnet'),
-//         ]).then((data) => {
-//             projectPath = path.join(MSDK_PATH, 'Examples', data[0], 'Bluetooth', PROJECT_DIR);
-//             return data;
-//         });
-//         console.log(target);
-//         console.log(dapSN);
-//         // let projectPath = path.join(MSDK_PATH, 'Examples', target, 'Bluetooth', PROJECT_DIR);
-//         console.log("PROJECT PATH: %s", projectPath);
-//         if (BUILD_FLAG) {
-//             await cleanProject(projectPath, DISTCLEAN_FLAG);
-//             await makeProject(projectPath);
-//         }
-//         let elfPath = path.join(projectPath, 'build', `${target.toLowerCase()}.elf`);
-//         retCode = await flashBoard(target, elfPath, dapSN, gdbPort, tclPort, telnetPort).then(
-//             (success) => { return procSuccess(success, 'Flash'); },
-//             (error) => { return procFail(error, 'Flash', true); }
-//         );
-//         if (retCode != 0) {
-//             await flashBoard(target, elfPath, dapSN, gdbPort, tclPort, telnetPort).then(
-//                 (success) => { return procSuccess(success, 'Flash'); },
-//                 (error) => { return procFail(error, 'Flash', false); }
-//             );
-//         }
-//     } else {
-//         console.log("!! ERROR: Improper permissions. Board could not be flashed. !!");
-//     }
-// }
 
 main();
