a7f554e8365d5b78833680b85f0d032646ec2ae0
abby wolf <Abigail.Wolf@analog.com>
2024-04-29 09:53:30 -0500

    final testing

--- actions/ocdreset/run.js
+++ actions/ocdreset/run.js
@@ -34,7 +34,6 @@ const resetBoard = function(target, dap, gdb, tcl, telnet) {
 }
 
 const main = async function () {
-    let owner = await getBoardOwner(BOARD_IDS);
     const targets = [];
     const dapSNs = [];
     const gdbPorts = [];
