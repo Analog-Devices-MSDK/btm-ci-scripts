e0929a77cdb4adffacabd72d88917d302f6d06ba
Lorne Smith <lorne.smith@analog.com>
2024-05-07 14:45:05 -0500

    fix print bug

--- tests/otas/otas_connected.py
+++ tests/otas/otas_connected.py
@@ -122,7 +122,8 @@ def client_test_discover_filespace(serial_port: serial.Serial) -> bool:
     while True:
         new_text = serial_port.read(serial_port.in_waiting).decode("utf-8")
         text += new_text
-        print(new_text)
+        if new_text:
+            print(new_text)
 
         if "File discovery complete" in text:
             return True
@@ -206,7 +207,8 @@ def client_verify_xfer(serial_port: serial.Serial) -> bool:
     while True:
         new_text = serial_port.read(serial_port.in_waiting).decode("utf-8")
         text += new_text
-        print(new_text)
+        if new_text:
+            print(new_text)
         status_match = re.search(pattern, text)
 
         # Check for successful completion
