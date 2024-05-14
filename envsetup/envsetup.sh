cbe13e47baaa704278d2f27109254fcbd2022a8f
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2024-05-14 11:28:04 -0500

    Cleanup ble wf (#21)
    * updated envsetup
    
    * add new runner
    
    ---------
    
    Co-authored-by: Lorne Smith <lorne.smith@analog.com>

--- envsetup/envsetup.sh
+++ envsetup/envsetup.sh
@@ -11,7 +11,7 @@ if is_not_running_as_root; then
 fi
 
 source /home/btm-ci/.bashrc
-git push --set-upstream origin exit-error
+
 env="LANG=en_US.UTF-16"
 printf '%s\n' \
     $env \
@@ -29,3 +29,7 @@ for i in {0..3}; do
     (cd /home/btm-ci/Workspace/btm-ci-github-runner$i && ./svc.sh start)
     pwd
 done
+
+cp .env /home/btm-ci/Workspace/adi-msdk-github-runner0
+(cd /home/btm-ci/Workspace/adi-msdk-github-runner0 && ./svc.sh stop)
+(cd /home/btm-ci/Workspace/adi-msdk-github-runner0 && ./svc.sh start)
