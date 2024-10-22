8935979cc61ca7b1765f0f9db6993364d6101394
crsz20 <Cristian.Cruz@analog.com>
2024-10-22 11:25:48 -0500

    Added CSV and PNG files to gitignore

--- tests/phy_verification/quick.py
+++
@@ -1,9 +0,0 @@
-import ble_test_suite.phy.rx_sensitivity as rx_sens
-from ble_test_suite.controllers import RxSensitivityTestController
-
-
-cfg = rx_sens.init_from_json('phyTest_master.json')
-ctrl = RxSensitivityTestController(cfg)
-
-ctrl.run_test()
-ctrl.results()
